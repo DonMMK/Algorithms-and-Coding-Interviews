b772aba46b3450b17db19ae8fd3c21d39eac1111
don <misurakalu@gmail.com>
2022-05-29 10:19:41 +1000

    Update CW8.py

--- Python/CW8.py
+++ Python/CW8.py
@@ -1,5 +1,5 @@
 # You will be given a number and you will need to return it as a string in Expanded Form. For example:
 def expanded_form(num):
 	n =1
-	stringAnswer = [line[i:i+n] for i in range(0, len(line), n)]    
+	stringAnswer = [num[i:i+n] for i in range(0, len(num), n)]    
 	return stringAnswer
