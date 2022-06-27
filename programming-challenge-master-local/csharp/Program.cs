77a0aeda4bc11429c1a3a6cbed0a71de1ef18dda
don <misurakalu@gmail.com>
2022-06-27 17:30:35 +1000

    Update

--- programming-challenge-master-local/csharp/Program.cs
+++ programming-challenge-master-local/csharp/Program.cs
@@ -119,34 +119,34 @@ namespace programming_challenge
 			Pig Latin Solver 4
 		****************************************************************************************************/
 
-				string vowels = "AEIOUaeiou";
-    			string cons = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
+			// 	string vowels = "AEIOUaeiou";
+    		// 	string cons = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
 
-    			Func<string, string> toPigLatin = word =>
-    			{
-	        		word = word.ToLower();
+    		// 	Func<string, string> toPigLatin = word =>
+    		// 	{
+	        // 		word = word.ToLower();
 
-	        		var result = word;
+	        // 		var result = word;
 
-	        	Func<string, string, (string, string)> split = (w, l) =>
-	        	{
-    	        	var prefix = new string(w.ToArray().TakeWhile(x => l.Contains(x)).ToArray());
-	            	return (prefix, w.Substring(prefix.Length));
-    	    	};
+	        // 	Func<string, string, (string, string)> split = (w, l) =>
+	        // 	{
+    	    //     	var prefix = new string(w.ToArray().TakeWhile(x => l.Contains(x)).ToArray());
+	        //     	return (prefix, w.Substring(prefix.Length));
+    	    // 	};
 
-    	    	if (!word.Any(w => cons.Contains(w)))
-    	    	{
-    	        	result = word + "way";
-    	    	}
-    	    	else
-    	    	{
-    	        	var (s, e) = split(word, vowels);
-    	        	var (s2, e2) = split(e, cons);
-    	        	result = e2 + s + s2 + "ay";
-    	    	}
-    	    		return result;
-    			};
-    		return string.Join(" ", english.Split(' ').Select(x => toPigLatin(x)));
+    	    // 	if (!word.Any(w => cons.Contains(w)))
+    	    // 	{
+    	    //     	result = word + "way";
+    	    // 	}
+    	    // 	else
+    	    // 	{
+    	    //     	var (s, e) = split(word, vowels);
+    	    //     	var (s2, e2) = split(e, cons);
+    	    //     	result = e2 + s + s2 + "ay";
+    	    // 	}
+    	    // 		return result;
+    		// 	};
+    		// return string.Join(" ", english.Split(' ').Select(x => toPigLatin(x)));
 
 
 		/****************************************************************************************************
